330ba51abfe161f0fafada546e3430b283ad7b93
Vitor Marini <vitorfmarini@gmail.com>
2026-08-31 10:13:01 -0300

    fix external netwrok acess

--- test/start_broker.sh
+++ test/start_broker.sh
@@ -10,7 +10,7 @@ docker rm mosquitto >/dev/null 2>&1 || true
 echo "[MQTT Test] Starting Mosquitto Broker Docker container on port 1883..."
 docker run -d \
   --name mosquitto \
-  -p 1883:1883 \
+  -p 0.0.0.0:1883:1883 \
   -v "${SCRIPT_DIR}/mosquitto.conf:/mosquitto/config/mosquitto.conf" \
   eclipse-mosquitto:2
 
